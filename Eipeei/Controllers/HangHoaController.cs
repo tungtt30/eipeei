88623b4f9d03f97ca90329009737c39d7cdf9257
tungtt30 <tungchipsctn2@gmail.com>
2022-06-28 13:47:14 +0700

    Add project files.

---
+++ Eipeei/Controllers/HangHoaController.cs
@@ -0,0 +1,53 @@
+﻿using Eipeei.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eipeei.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                // LINQ [Object] query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+
+        }
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hanghoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+            hangHoas.Add(hanghoa);
+            return Ok(new
+            {
+                success = true,
+                data = hanghoa
+            });
+        }
+    }
+}
