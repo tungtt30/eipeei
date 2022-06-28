41ae63d34ae5518c8d9107d18ee7e926a8e8768b
tungtt30 <tungchipsctn2@gmail.com>
2022-06-28 14:19:45 +0700

    durc

--- Eipeei/Controllers/HangHoaController.cs
+++ Eipeei/Controllers/HangHoaController.cs
@@ -33,6 +33,8 @@ namespace Eipeei.Controllers
             }
 
         }
+
+
         [HttpPost]
         public IActionResult Create(HangHoaVM hangHoaVM)
         {
@@ -48,6 +50,57 @@ namespace Eipeei.Controllers
                 success = true,
                 data = hanghoa
             });
+        }
+
+        [HttpPut("{id})")]
+        public IActionResult Edit(string id, HangHoa hanghoaEdit)
+        {
+            try
+            {
+                // LINQ [Object] query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                // update
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                hangHoa.TenHangHoa = hanghoaEdit.TenHangHoa;
+                hangHoa.DonGia = hanghoaEdit.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                // LINQ [Object] query
+                var hangHoa = hangHoas.SingleOrDefault(hh => hh.MaHangHoa == Guid.Parse(id));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                // update
+                if (id != hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
